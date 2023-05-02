bca762988ba56ec2e06f51bcd980222c26eb4149
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-02 17:11:05 -0500

    defined columns for the category and product models

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns( id:,category_name:)
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,41 @@ class Product extends Model {}
 Product.init(
   {
     // define columns (//id:, product_name:,price:, stock; category_id: )
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
+
   {
     sequelize,
     timestamps: false,
