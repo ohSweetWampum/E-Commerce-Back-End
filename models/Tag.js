74af460f990701d55e801cad7d5e17bc57a88b41
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-02 17:14:51 -0500

    defined columns for the producttag and tag model files

--- models/Tag.js
+++ models/Tag.js
@@ -7,7 +7,18 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns( id:, tag_name:)
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
+
   {
     sequelize,
     timestamps: false,
