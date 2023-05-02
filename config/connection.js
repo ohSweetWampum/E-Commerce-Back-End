10f014ea0f08c9e93a1130a435d701960954d03c
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-02 08:53:38 -0500

    starter code

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const Sequelize = require("sequelize");
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        //(P@ssw0rd123!)
+        host: "localhost",
+        dialect: "mysql",
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
+
+module.exports = sequelize;
