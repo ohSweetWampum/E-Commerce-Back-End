0e676bbe77ff287b43d1d7b6835f892bf193ea38
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-03 10:22:45 -0500

    completed all routes in the product-routes.js file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -17,10 +17,24 @@ router.get("/", async (req, res) => {
   }
 });
 
+// find a single product by its `id`
+// be sure to include its associated Category and Tag data
 // get one product
-router.get("/:id", (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get("/:id", async (req, res) => {
+  try {
+    const ProductData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+    });
+
+    if (!ProductData) {
+      res.status(404).json({ message: "No product found with that id!" });
+      return;
+    }
+
+    res.status(200).json(ProductData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
