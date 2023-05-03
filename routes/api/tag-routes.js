493eaffb6e559c3bb31ed9c908c9439b1ecfe3ae
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-03 10:37:39 -0500

    forgot to include m" through Product Tah for the find all tags and find single tag routes in the tag-routes file, so I added them

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,9 @@ const { Tag, Product, ProductTag } = require("../../models");
 
 router.get("/", async (req, res) => {
   try {
-    const tagData = await Tag.findAll();
+    const tagData = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag }],
+    });
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
@@ -21,7 +23,7 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }],
+      include: [{ model: Product, through: ProductTag }],
     });
 
     if (!tagData) {
